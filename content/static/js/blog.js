4fe6704e728bb7cd82afd540f0132c0255d66dcf
Leonard Lorenz <dev@leonardlorenz.de>
2020-11-02 16:04:22 +0100

    fixed token setting

--- content/static/js/blog.js
+++ content/static/js/blog.js
@@ -2,13 +2,20 @@ function setTokenCookie() {
     let token = document.getElementById('set-token').value;
     let tokenCookie = 'token=' + token + "; SameSite=None; secure";
     document.cookie = tokenCookie;
-    document.getElementById("token").value = token
+    setFormTokens(token);
     document.getElementById("cookie-block").hidden = true;
 }
 
 function clearTokenCookie() {
     document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; SameSite=None; secure";
     document.getElementById("cookie-block").hidden = false;
+}
+
+function setFormTokens(token) {
+    let tokenFields = document.querySelectorAll(".token");
+    for (t of tokenFields) {
+        t.value = token;
+    }
 }
 
 // if cookie is set, use it to pass the token
@@ -18,10 +25,7 @@ for (c of c_pairs) {
     if (c.trim().split("=")[0].startsWith("token")){
         // stick token into all the form input fields
         let token = c.split("=")[1];
-        let tokenFields = document.querySelectorAll(".token");
-        for (t of tokenFields) {
-            t.value = token;
-        }
+        setFormTokens(token);
         cookie_set = true;
     }
 }
